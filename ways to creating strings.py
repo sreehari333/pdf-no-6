823f6cd48048493d675cddc768156c7bb28efe49
sreehari333 <85100069+sreehari333@users.noreply.github.com>
2021-06-03 14:21:35 +0530

    Add files via upload

---
+++ ways to creating strings.py
@@ -0,0 +1,17 @@
+#Strings can be created by enclosing characters inside a single quote or double-quotes.
+# Even triple quotes can be used in Python but generally used to represent multiline strings and docstrings.
+
+
+my_string = 'Hello'
+print(my_string)
+
+my_string = "Hello"
+print(my_string)
+
+my_string = '''Hello'''
+print(my_string)
+
+# triple quotes string can extend multiple lines
+my_string = """Hello, welcome to
+           the world of Python"""
+print(my_string)
